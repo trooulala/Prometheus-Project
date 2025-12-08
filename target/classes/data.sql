4e53009ebda330b31fb2f9f7fff94a52f434b74a
trooulala <trooulala@gmail.com>
2025-12-08 22:36:12 +0700

    Create data.sql

---
+++ target/classes/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO employees (id, workemail, name, password, birthdate, joindate, employeetype)
+VALUES
+('ADM-1', 'admin@prometheus.co.id', 'Super Admin', '$2a$12$SGsG6xqkc3hrmbkJV0nvJu5LRwuaF0r1QZyDgXCiiuoFdFVbLZeSy', '1990-01-15', '2020-01-10', 'ADMINISTRATOR'),
+
+('PRG-1', 'andi.prg@prometheus.co.id', 'Andi Programmer', '$2a$12$SGsG6xqkc3hrmbkJV0nvJu5LRwuaF0r1QZyDgXCiiuoFdFVbLZeSy', '1998-03-12', '2023-01-10', 'PROGRAMMER'),
+('PRG-2', 'budi.prg@prometheus.co.id', 'Budi Programmer', '$2a$12$SGsG6xqkc3hrmbkJV0nvJu5LRwuaF0r1QZyDgXCiiuoFdFVbLZeSy', '1997-07-21', '2023-06-15', 'PROGRAMMER'),
+
+('REC-1', 'sinta.rec@prometheus.co.id', 'Sinta Recruiter', '$2a$12$SGsG6xqkc3hrmbkJV0nvJu5LRwuaF0r1QZyDgXCiiuoFdFVbLZeSy', '1999-05-09', '2024-01-10', 'RECRUITER'),
+
+('SAL-1', 'rio.sal@prometheus.co.id', 'Rio Salesman', '$2a$12$SGsG6xqkc3hrmbkJV0nvJu5LRwuaF0r1QZyDgXCiiuoFdFVbLZeSy', '1996-11-30', '2023-02-01', 'SALESMAN'),
+('SAL-2', 'dina.sal@prometheus.co.id', 'Dina Salesman', '$2a$12$SGsG6xqkc3hrmbkJV0nvJu5LRwuaF0r1QZyDgXCiiuoFdFVbLZeSy', '1997-09-18', '2024-03-10', 'SALESMAN');
