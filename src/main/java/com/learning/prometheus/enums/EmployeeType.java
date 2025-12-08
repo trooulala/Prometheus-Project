5c23286337709d9b8f247ca43d9567ace817a88c
trooulala <trooulala@gmail.com>
2025-12-08 22:35:32 +0700

    ad ADMINISTRATOR type

--- src/main/java/com/learning/prometheus/enums/EmployeeType.java
+++ src/main/java/com/learning/prometheus/enums/EmployeeType.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum EmployeeType {
+    ADMINISTRATOR("ADM"),
     PROGRAMMER("PRG"),
     RECRUITER("REC"),
     SALESMAN("SAL");
